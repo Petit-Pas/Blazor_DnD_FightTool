6f06fdd959ef3a784ed3c9cf5252d9d1df41abff
Benoit Hoffman <benoit.hoffman@satellit.be>
2022-12-23 20:15:50 +0100

    WIP playing with inputs

---
+++ DnDBlazorComponents/Characteristics.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using NeoBlazorphic.StyleParameters;
+
+namespace DnDBlazorComponents
+{
+    public partial class Characteristics : ComponentBase
+    {
+
+
+        // UI Methods
+
+        private static BorderRadius _round { get; set; } = new BorderRadius(5, "em");
+        public BorderRadius GetBonusMarkerBorderRadius()
+        {
+            return _round;
+        }
+    }
+}
---
+++ DnDFightTool/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DnDFightTool;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
