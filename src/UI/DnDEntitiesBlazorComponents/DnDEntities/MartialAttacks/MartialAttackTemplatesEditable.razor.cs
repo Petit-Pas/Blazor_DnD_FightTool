632f0a1e84d80b548b3828c2411e37af375dcfd0
Benoit Hoffman <benoit.hoffman@satellit.be>
2023-08-12 21:57:00 +0200

    Edited Attack template

--- src/UI/DnDEntitiesBlazorComponents/DnDEntities/MartialAttacks/MartialAttackTemplatesEditable.razor.cs
+++ src/UI/DnDEntitiesBlazorComponents/DnDEntities/MartialAttacks/MartialAttackTemplatesEditable.razor.cs
@@ -1,5 +1,6 @@
 ﻿using DnDFightTool.Domain.DnDEntities.Characters;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace DnDEntitiesBlazorComponents.DnDEntities.MartialAttacks;
 
@@ -7,5 +8,21 @@ public partial class MartialAttackTemplatesEditable
 {
     [Parameter]
     public Character Character { get; set; }
+
+    private EditContext _editContext { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        if (Character != null)
+        {
+            _editContext = new EditContext(Character);
+
+            //_editContext.OnFieldChanged += (sender, args) =>
+            //{
+            //    StateHasChanged();
+            //};
+        }
+    }
 
 }
