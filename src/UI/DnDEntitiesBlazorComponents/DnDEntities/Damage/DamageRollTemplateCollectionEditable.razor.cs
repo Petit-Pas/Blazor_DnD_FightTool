ed5e4231b36ba3911d27daff517ace01789131b8
Benoit Hoffman <benoit.hoffman@satellit.be>
2024-02-10 15:20:28 +0100

    Enhanced damage roll template for attacks

---
+++ src/UI/DnDEntitiesBlazorComponents/DnDEntities/Damage/DamageRollTemplateCollectionEditable.razor.cs
@@ -0,0 +1,11 @@
+﻿using DnDFightTool.Domain.DnDEntities.Damage;
+using Microsoft.AspNetCore.Components;
+
+namespace DnDEntitiesBlazorComponents.DnDEntities.Damage
+{
+    public partial class DamageRollTemplateCollectionEditable
+    {
+        [Parameter]
+        public DamageRollTemplateCollection? DamageRollTemplateCollection { get; set; }
+    }
+}
