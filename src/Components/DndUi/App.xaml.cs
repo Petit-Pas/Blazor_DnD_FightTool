321f28ebf460e739427b44d7acd0b39b59081c7f
Benoit Hoffman <benoit.hoffman@satellit.be>
2025-07-09 22:55:08 +0200

    Basic example with MudBlazor and WebView setup for a clean slate on UI

---
+++ src/Components/DndUi/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace DndUi;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "DndUi" };
+	}
+}
---
+++ src/Components/DndUi/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DndUi;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
