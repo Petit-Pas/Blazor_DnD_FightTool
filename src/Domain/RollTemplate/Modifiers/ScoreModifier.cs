e9589b731dd6b6d98c52c7f21d5c516e44158551
Benoit Hoffman <benoit.hoffman@satellit.be>
2026-04-23 16:43:56 +0200

    wip

---
+++ src/Domain/RollTemplate/Modifiers/ScoreModifier.cs
@@ -0,0 +1,47 @@
+namespace DnDFightTool.Domain.RollTemplate.Modifiers;
+
+/// <summary>
+///     Modifier meant to simply add an amount to a roll/score/whatever
+/// </summary>
+/// <param name="Modifier"> By how much it should increase the score </param>
+public record ScoreModifier(int Modifier)
+{
+    /// <summary>
+    ///    Empty modifier
+    /// </summary>
+    public readonly static ScoreModifier Empty = new(0);
+
+    /// <summary>
+    ///     Add + sign to the modifier when needed.
+    /// </summary>
+    public string ModifierString => (Modifier >= 0 ? "+" : "") + Modifier;
+
+    /// <summary>
+    ///     Applies the modifier to the provided value
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public int ApplyTo(int value)
+    {
+        return value + Modifier;
+    }
+
+    /// <summary>
+    ///     Implicit conversion to int to allow to use the modifier directly in a calculation
+    /// </summary>
+    /// <param name="modifier"></param>
+    public static implicit operator int(ScoreModifier modifier)
+    {
+        return modifier.Modifier;
+    }
+
+    public static implicit operator ScoreModifier(int value)
+    {
+        return new ScoreModifier(value);
+    }
+
+    public override string ToString()
+    {
+        return ModifierString;
+    }
+}
